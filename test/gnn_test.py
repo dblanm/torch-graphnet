7f1eae42b173e883fe3b95226c505059dc92d87e
David Blanco Mulero <david.blancomulero@aalto.fi>
2021-04-26 17:55:27 +0300

    Refactor test with edge_index, GNData class built in test.

--- test/gnn_test.py
+++ test/gnn_test.py
@@ -8,10 +8,11 @@ import numpy as np
 import tensorflow as tf
 import torch
 import torch_scatter
+from torch_geometric.data import Data
 
 from torch_graphnet.utils import receiver_nodes_to_edges, sender_nodes_to_edges, \
     received_edges_to_node_aggregator, sent_edges_to_node_aggregator, context_to_nodes,\
-    context_to_edges, GNData
+    context_to_edges
 
 from torch_graphnet import EdgeModel, NodeModel, InteractionNetwork
 
@@ -20,14 +21,44 @@ np.random.seed(seed)
 tf.random.set_seed(seed)
 torch.manual_seed(seed)
 
+
+class GNData(Data):
+    def __init__(self, x: torch.Tensor = None, edge_index: torch.Tensor = None,
+                 edge_attr: torch.Tensor = None, global_context: torch.Tensor = None,
+                 y: torch.Tensor = None, pos: torch.Tensor = None,
+                 normal: torch.Tensor = None, face: torch.Tensor = None, **kwargs):
+        super(GNData, self).__init__(x=x, edge_index=edge_index, edge_attr=edge_attr,
+                                     y=y, pos=pos, normal=normal, face=face, kwargs=kwargs)
+        # Convert the receivers/senders to int64 and reshape to be a 2-D vector
+        self.global_context = global_context
+
+    @property
+    def nodes(self):
+        return self.x
+
+    @property
+    def senders(self):
+        return self.edge_index[0, :]
+
+    @property
+    def receivers(self):
+        return self.edge_index[1, :]
+
+    @property
+    def num_global(self):
+        return self.global_context.shape[-1]
+
 def from_graphstuple_to_gndata(graph: gn.graphs.GraphsTuple):
     # TODO Create from GraphsTuple to GNData
     nodes = torch.from_numpy(graph.nodes.numpy())
     edges = torch.from_numpy(graph.edges.numpy())
-    receivers = torch.from_numpy(graph.receivers.numpy())
-    senders = torch.from_numpy(graph.senders.numpy())
+    receivers = torch.from_numpy(graph.receivers.numpy()).long()
+    senders = torch.from_numpy(graph.senders.numpy()).long()
     globals = torch.from_numpy(graph.globals.numpy())
-    torch_graph = GNData(x=nodes, receiver_nodes=receivers, sender_nodes=senders,
+
+    edge_index = torch.stack((senders,receivers)).long()
+
+    torch_graph = GNData(x=nodes, edge_index=edge_index.contiguous(),
                          edge_attr=edges, global_context=globals)
     return torch_graph
 
@@ -75,17 +106,21 @@ def create_graph():
 
     # Node features for graph 1.
     nodes_1 = [[1010., 1020., 1030.],  # Node 0
-               [1011., 1021., 1031.]]  # Node 1
+               [1012., 1020., 1030.],  # Node 1
+               [1013., 1020., 1030.],  # Node 2
+               [1014., 1020., 1030.],  # Node 3
+               [1015., 1021., 1031.]]  # Node 4
 
     # Edge features for graph 1.
     edges_1 = [[1100., 1200.],  # Edge 0
                [1101., 1201.],  # Edge 1
                [1102., 1202.],  # Edge 2
-               [1103., 1203.]]  # Edge 3
+               [1102., 1202.],  # Edge 3
+               [1103., 1203.]]  # Edge 4
 
     # The sender and receiver nodes associated with each edge for graph 1.
-    senders_1 = [0, 0, 1, 1]
-    receivers_1 = [0, 1, 0, 0]
+    senders_1 = [0, 1, 2, 3, 4]
+    receivers_1 = [1, 2, 3, 4, 3]
 
     data_dict_0 = {"globals": np.array(globals_0), "nodes": np.array(nodes_0),
                    "edges": np.array(edges_0),
@@ -161,7 +196,7 @@ def test_blocks(graphs_tuple: gn.graphs.GraphsTuple):
         out_edge_block_tf = edge_block_tf(graph_gn)
         edges_tf = out_edge_block_tf.edges
         out_edge_block_torch = edge_block_torch(graph.nodes, graph.edge_attr,
-                                                graph.senders, graph.receivers).detach()
+                                                graph.edge_index).detach()
 
         np.testing.assert_allclose(edges_tf.numpy(), out_edge_block_torch.numpy(),
                                    err_msg="Edge block does not match")
@@ -181,7 +216,7 @@ def test_blocks(graphs_tuple: gn.graphs.GraphsTuple):
         out_node_block_tf = node_block_tf(graph_gn)
         nodes_tf = out_node_block_tf.nodes
         out_node_block_torch = node_block_torch(graph.nodes, graph.edge_attr,
-                                                graph.senders, graph.receivers).detach()
+                                                graph.edge_index).detach()
 
         np.testing.assert_allclose(nodes_tf.numpy(), out_node_block_torch.numpy(),
                                    err_msg="Node block does not match")
@@ -192,9 +227,9 @@ def test_blocks(graphs_tuple: gn.graphs.GraphsTuple):
     test_node_block(graph_torch, graph_tf)
 
 
-def test_gns(graphs_tuple: gn.graphs.GraphsTuple):
+def test_in(graphs_tuple: gn.graphs.GraphsTuple):
 
-    def test_interaction_network(graph: GNData, graph_gn: gn.graphs.GraphsTuple,
+    def test_interaction_network_forward(graph: GNData, graph_gn: gn.graphs.GraphsTuple,
                                  node_fn_tf, node_fn_torch, edge_fn_tf, edge_fn_torch):
 
         INet_tf = gn.modules.InteractionNetwork(edge_model_fn=edge_fn_tf,
@@ -203,39 +238,73 @@ def test_gns(graphs_tuple: gn.graphs.GraphsTuple):
                                         phi_node=node_fn_torch)
 
         out_graph_tf = INet_tf(graph_gn)
-        node_out, edge_out, _, _ = INet_torch(graph.nodes, graph.edge_attr,
-                                        graph.senders, graph.receivers)
+        node_out, edge_out, _ = INet_torch(graph.nodes, graph.edge_attr,
+                                        graph.edge_index)
 
         np.testing.assert_allclose(out_graph_tf.nodes.numpy(), node_out.detach().numpy(),
                                    err_msg="Interaction network Nodes output does not match")
         np.testing.assert_allclose(out_graph_tf.edges.numpy(), edge_out.detach().numpy(),
                                    err_msg="Interaction network Edges output does not match")
+        print("Interaction Network forward passed")
+
+        return INet_tf, INet_torch
+
+    def test_interaction_network_backward(inet_tf, inet_torch, graph_in_tf, graph_tgt_tf,
+                                          graph_in_torch, graph_tgt_torch):
+
+        with tf.GradientTape() as tape:
+            loss_tf = tf.reduce_mean(tf.square(inet_tf(graph_in_tf).nodes - graph_tgt_tf.nodes))
+            # loss_value = loss_fn(inet_tf, graph_in_tf, graph_tgt_tf)
+            grads = tape.gradient(loss_tf, inet_tf.trainable_variables)
+        grad_tf_phi_edge_b = grads[0].numpy()
+        grad_tf_phi_edge_w = grads[1].numpy()
+        grad_tf_phi_node_b = grads[2].numpy()
+        grad_tf_phi_node_w = grads[3].numpy()
+        node_out, edge_out, _ = inet_torch(graph_in_torch.nodes, graph_in_torch.edge_attr,
+                                              graph_in_torch.edge_index)
+
+        criterion = torch.nn.MSELoss()
+        loss = criterion(node_out, graph_tgt_torch.nodes)
+        loss.backward()
+        grad_torch_phi_node_w = inet_torch.phi_node.weight.grad.detach().numpy().T
+        grad_torch_phi_node_b = inet_torch.phi_node.bias.grad.detach().numpy()
+        grad_torch_phi_edge_w = inet_torch.phi_edge.weight.grad.detach().numpy().T
+        grad_torch_phi_edge_b = inet_torch.phi_edge.bias.grad.detach().numpy()
+
+        np.testing.assert_allclose(grad_tf_phi_node_w, grad_torch_phi_node_w,
+                                   err_msg="Phi node weights gradient does not match")
+        np.testing.assert_allclose(grad_tf_phi_node_b, grad_torch_phi_node_b,
+                                   err_msg="Phi node bias gradient does not match")
+        np.testing.assert_allclose(grad_tf_phi_edge_w, grad_torch_phi_edge_w,
+                                   err_msg="Phi edge weights gradient does not match")
+        np.testing.assert_allclose(grad_tf_phi_edge_b, grad_torch_phi_edge_b,
+                                   err_msg="Phi edge bias gradient does not match")
+        print("Interaction network gradient passed")
+
+
 
 
     graph_tf = gn.utils_tf.get_graph(graphs_tuple, index=0)
+    graph_tgt_tf = gn.utils_tf.get_graph(graphs_tuple, index=1)
     graph = from_graphstuple_to_gndata(graph_tf)
+    graph_tgt = from_graphstuple_to_gndata(graph_tgt_tf)
     edge_size = graph.num_edge_features + graph.num_node_features + graph.num_node_features
-    edge_out = 5
+    edge_out = 3
     node_size = graph.num_node_features + edge_out
-    node_out = 5
+    node_out = 3
 
     node_fn_tf, node_fn_torch = get_model_fn(node_size, node_out)
     edge_fn_tf, edge_fn_torch = get_model_fn(edge_size, edge_out)
 
-    test_interaction_network(graph, graph_tf, node_fn_tf, node_fn_torch,
-                             edge_fn_tf, edge_fn_torch)
+    INet_tf, INet_torch = test_interaction_network_forward(graph, graph_tf, node_fn_tf, node_fn_torch,
+                                                           edge_fn_tf, edge_fn_torch)
+    test_interaction_network_backward(INet_tf, INet_torch, graph_tf, graph_tgt_tf, graph, graph_tgt)
 
 
-def build_and_test_gnn_torch(graphs_tuple, graph_net_tf):
-    # Get the output for each graph
-    latent_1_tf = graph_net_tf(gn.utils_tf.get_graph(graphs_tuple, index=0))
-    latent_2_tf = graph_net_tf(gn.utils_tf.get_graph(graphs_tuple, index=1))
-    # Get the parameters of the GN TF
-    gnn_params = [var.numpy() for var in graph_net_tf.variables]
 
 if __name__ == "__main__":
     graphs_tuple = create_graph()
     test_gn_utils(graphs_tuple)
     test_blocks(graphs_tuple)
-    test_gns(graphs_tuple)
+    test_in(graphs_tuple)
     print("Everything passed")
